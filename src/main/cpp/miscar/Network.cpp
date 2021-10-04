a7a958fffa381cbfe6df7a48652e0c7be9f251bb
Noam Zaks <imnoamzaks@gmail.com>
2021-10-04 19:52:54 +0300

    Fix network interface to specify which typenames can be sent

--- src/main/cpp/miscar/Network.cpp
+++ src/main/cpp/miscar/Network.cpp
@@ -10,60 +10,106 @@
 #define TABLE_PREFIX "/MisCar/"
 
 template <>
-bool miscar::network::Get<bool>(const std::string &path) {
+bool miscar::network::Get(const std::string &path) {
   return nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .GetBoolean(false);
 }
 
 template <>
-int miscar::network::Get<int>(const std::string &path) {
+int miscar::network::Get(const std::string &path) {
   return nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .GetDouble(0);
 }
 
 template <>
-double miscar::network::Get<double>(const std::string &path) {
+double miscar::network::Get(const std::string &path) {
   return nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .GetDouble(0);
 }
 
 template <>
-std::string miscar::network::Get<std::string>(const std::string &path) {
+std::string miscar::network::Get(const std::string &path) {
   return nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .GetString("");
 }
 
 template <>
-void miscar::network::Set<bool>(const std::string &path, bool value) {
+std::vector<bool> miscar::network::Get(const std::string &path) {
+  auto result = nt::NetworkTableInstance::GetDefault()
+                    .GetEntry(TABLE_PREFIX + path)
+                    .GetBooleanArray({});
+
+  return std::vector<bool>(result.begin(), result.end());
+}
+
+template <>
+std::vector<int> miscar::network::Get(const std::string &path) {
+  auto result = nt::NetworkTableInstance::GetDefault()
+                    .GetEntry(TABLE_PREFIX + path)
+                    .GetDoubleArray({});
+
+  return std::vector<int>(result.begin(), result.end());
+}
+
+template <>
+void miscar::network::Set(const std::string &path, bool value) {
   nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .SetBoolean(value);
 }
 
 template <>
-void miscar::network::Set<int>(const std::string &path, int value) {
+void miscar::network::Set(const std::string &path, int value) {
   nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .SetDouble(value);
 }
 
 template <>
-void miscar::network::Set<double>(const std::string &path, double value) {
+void miscar::network::Set(const std::string &path, double value) {
   nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .SetDouble(value);
 }
 
 template <>
-void miscar::network::Set<std::string>(const std::string &path,
-                                       std::string value) {
+void miscar::network::Set(const std::string &path, std::string value) {
   nt::NetworkTableInstance::GetDefault()
       .GetEntry(TABLE_PREFIX + path)
       .SetString(value);
+}
+
+template <>
+void miscar::network::Set(const std::string &path, std::vector<bool> value) {
+  nt::NetworkTableInstance::GetDefault()
+      .GetEntry(TABLE_PREFIX + path)
+      .SetBooleanArray(std::vector<int>(value.begin(), value.end()));
+}
+
+template <>
+void miscar::network::Set(const std::string &path, std::vector<int> value) {
+  nt::NetworkTableInstance::GetDefault()
+      .GetEntry(TABLE_PREFIX + path)
+      .SetDoubleArray(std::vector<double>(value.begin(), value.end()));
+}
+
+template <>
+void miscar::network::Set(const std::string &path, std::vector<double> value) {
+  nt::NetworkTableInstance::GetDefault()
+      .GetEntry(TABLE_PREFIX + path)
+      .SetDoubleArray(value);
+}
+
+template <>
+void miscar::network::Set(const std::string &path,
+                          std::vector<std::string> value) {
+  nt::NetworkTableInstance::GetDefault()
+      .GetEntry(TABLE_PREFIX + path)
+      .SetStringArray(value);
 }
 
 void miscar::network::Add(const std::string &path, frc::Sendable &sendable) {
--- src/main/cpp/miscar/Network.h
+++ src/main/cpp/miscar/Network.h
@@ -10,11 +10,59 @@ namespace miscar::network {
 
 /** Get a value from the Network Tables. */
 template <typename T>
-T Get(const std::string& path);
+T Get(const std::string& path) = delete;
+
+template <>
+bool Get(const std::string& path);
+
+template <>
+int Get(const std::string& path);
+
+template <>
+double Get(const std::string& path);
+
+template <>
+std::vector<bool> Get(const std::string& path);
+
+template <>
+std::vector<int> Get(const std::string& path);
+
+template <>
+std::vector<double> Get(const std::string& path);
+
+template <>
+std::vector<std::string> Get(const std::string& path);
+
+template <>
+std::string Get(const std::string& path);
 
 /** Set a value in the Network Tables. */
 template <typename T>
-void Set(const std::string& path, T value);
+void Set(const std::string& path, T value) = delete;
+
+template <>
+void Set(const std::string& path, bool value);
+
+template <>
+void Set(const std::string& path, int value);
+
+template <>
+void Set(const std::string& path, double value);
+
+template <>
+void Set(const std::string& path, std::string value);
+
+template <>
+void Set(const std::string& path, std::vector<bool> value);
+
+template <>
+void Set(const std::string& path, std::vector<int> value);
+
+template <>
+void Set(const std::string& path, std::vector<double> value);
+
+template <>
+void Set(const std::string& path, std::vector<std::string> value);
 
 /** Add a sendable to Network Tables. */
 void Add(const std::string& path, frc::Sendable& sendable);
