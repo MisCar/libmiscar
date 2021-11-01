a202227f4b5736885cb858b365b73fb93e8578d8
Noam Zaks <imnoamzaks@gmail.com>
2021-11-01 09:08:52 +0200

    Quickfix PID for constexprs

--- src/main/cpp/miscar/PID.h
+++ src/main/cpp/miscar/PID.h
@@ -13,8 +13,9 @@ struct PID {
   double integral_zone;
   int slot;
 
-  constexpr PID(double p, double i, double d, double f, double integral_zone = 0,
-      int slot = 0);
+  constexpr PID(double p, double i, double d, double f,
+                double integral_zone = 0, int slot = 0)
+      : p(p), i(i), d(d), f(f), integral_zone(integral_zone), slot(slot) {}
 };
 
 }  // namespace miscar
