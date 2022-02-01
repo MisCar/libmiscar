882ca9a0a2459c3bbec51c05d5e976100426b83c
MisCar Programming <programming@miscar1574.org>
2022-02-01 18:59:48 +0200

    feat: added minus in y axis

--- src/main/cpp/miscar/joystick/PS4Controller.cpp
+++ src/main/cpp/miscar/joystick/PS4Controller.cpp
@@ -17,11 +17,11 @@ double miscar::PS4Controller::GetRightX() const {
 }
 
 double miscar::PS4Controller::GetLeftY() const {
-  return GetRawAxis(Axis::kLeftY);
+  return -GetRawAxis(Axis::kLeftY);
 }
 
 double miscar::PS4Controller::GetRightY() const {
-  return GetRawAxis(Axis::kRightY);
+  return -GetRawAxis(Axis::kRightY);
 }
 
 double miscar::PS4Controller::GetL2Axis() const {
